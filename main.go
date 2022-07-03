56d088aedf300c342887b62c86c496213fef874f
Maik Schmaddebeck <maik.schmaddebeck@icloud.com>
2022-07-03 22:35:57 +0200

    Added bubbletea

--- main.go
+++ main.go
@@ -3,9 +3,15 @@ package main
 import (
 	"bytes"
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
+
+	// "github.com/charmbracelet/bubbles/key"
+	// "github.com/charmbracelet/bubbles/list"
+	tea "github.com/charmbracelet/bubbletea"
+	// "github.com/charmbracelet/lipgloss"
 )
 
 type Request struct {
@@ -24,6 +30,44 @@ type Data struct {
 	Requests []Request `json:"requests"`
 }
 
+type Model struct {
+	Selected int
+	Requests  []Request
+}
+
+// var appStyle = lipgloss.NewStyle().Padding(1, 2)
+
+func (m Model) Init() tea.Cmd {
+	return tea.EnterAltScreen
+}
+
+func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "ctrl+c", "q":
+			return m, tea.Quit
+		}
+	}
+
+	return m, nil
+}
+
+func (m Model) View() string {
+	view := ""
+
+	for idx, request := range m.Requests {
+		if idx == m.Selected {
+			view += ">"
+		} else {
+			view += " "
+		}
+		view += fmt.Sprintf(" %s (%s)\n", request.URL, request.Method)
+	}
+
+	return view
+}
+
 func ReadResponse(response *http.Response) string {
 	defer response.Body.Close()
 
@@ -33,24 +77,6 @@ func ReadResponse(response *http.Response) string {
 	}
 
 	return string(body)
-}
-
-func main() {
-	log.Println("Postbote start")
-
-	file, err := ioutil.ReadFile("postbote.json")
-	if err != nil {
-		panic(err)
-	}
-
-	data := new(Data)
-	if err := json.Unmarshal(file, &data); err != nil {
-		panic(err)
-	}
-
-	for _, request := range data.Requests {
-		Send(request)
-	}
 }
 
 func CreateQueryURL(req *http.Request, params map[string]interface{}) string {
@@ -66,7 +92,7 @@ func CreateQueryURL(req *http.Request, params map[string]interface{}) string {
 		}
 	}
 	return query.Encode()
-} 
+}
 
 func Send(request Request) {
 	body := new(bytes.Buffer)
@@ -82,7 +108,7 @@ func Send(request Request) {
 	if err != nil {
 		panic(err)
 	}
-	
+
 	req.URL.RawQuery = CreateQueryURL(req, request.Params)
 
 	if request.ContentType != "" {
@@ -98,4 +124,32 @@ func Send(request Request) {
 
 	response := ReadResponse(res)
 	log.Println(response)
+}
+
+func main() {
+	log.Println("Postbote start")
+
+	file, err := ioutil.ReadFile("postbote.json")
+	if err != nil {
+		panic(err)
+	}
+
+	data := new(Data)
+	if err := json.Unmarshal(file, &data); err != nil {
+		panic(err)
+	}
+
+	model := &Model{
+		Requests: data.Requests,
+		Selected: 0,
+	}
+	program := tea.NewProgram(model)
+
+	if err := program.Start(); err != nil {
+		log.Fatalln(err)
+	}
+
+	// for _, request := range data.Requests {
+	// 	Send(request)
+	// }
 }
